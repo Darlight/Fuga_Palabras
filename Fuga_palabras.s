43bdb370278cf75515235b5be354e88bd6c7fcb6
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2019-04-25 08:20:02 -0600

    .

--- Fuga_palabras.s
+++ Fuga_palabras.s
@@ -18,6 +18,9 @@ formato_mal1:	.asciz "ERROR. Puntos Jugador 1: %d\n"
 formato_mal2:	.asciz "ERROR. Puntos Jugador 2: %d\n"
 formato_bien1:	.asciz "BIEN. Puntos Jugador 1: %d\n"
 formato_bien2:	.asciz "BIEN. Puntos Jugador 2: %d\n"
+ganador1:	.asciz "El Jugador 1 ha ganado"
+ganador2:	.asciz "El Jugador 2 ha ganado"
+empates:	.asciz "HA SIDO UN EMPATE"
 
 pal1:	
 	.asciz "p","a","c","t","o","_"
@@ -67,3 +70,84 @@ pal20:
 
 main:
 
+turno1:
+	SUB contador_palabras, contador_palabras, #1
+	LDR R0, =formato_vocal
+	BL puts
+
+	BL getchar
+	LDR r1,=formato_vocal
+	STR r0,[r1]
+
+	@Compara la letra ingresada con la letra faltante
+	CMP R0, letra
+	BEQ correcto1
+
+correcto1:
+	@Suma 5 puntos al contador de puntos del jugador 1
+	add contador1, contador1, #5
+	bl turno2
+
+incorrecto1:
+	@Resta 2 puntos al contador de puntos del jugador 1
+	sub contador1, contador1, #2
+	bl turno2
+
+turno2:
+	SUB contador_palabras, contador_palabras, #1
+
+	LDR R0, =formato_vocal
+	BL puts
+
+	BL getchar
+	LDR r1,=formato_vocal
+	STR r0,[r1]
+
+	@Compara la letra ingresada con la letra faltante
+	CMP R0, letra
+	BEQ correcto2
+
+correcto2:
+	@Suma 5 puntos al contador de puntos del jugador 2
+	add contador2, contador2, #5
+	bl comparar
+
+incorrecto2:
+	@Resta 2 puntos al contador de puntos del jugador 2
+	sub contador2, contador2, #2
+	bl comparar
+
+comparar:
+	CMP contador_palabras, #0
+	BEQ fin
+	BNE turno1
+
+ganador:
+	CMP contador1, contador2
+	BGT gana1
+	BLT gana2
+	BEQ empate
+
+gana1:
+	LDR R0, =ganador1
+	LDR R0, [R0]
+	bl puts
+	bl fin
+
+gana2:
+	LDR R0, =ganador2
+	LDR R0, [R0]
+	bl puts
+	bl fin
+
+empate:
+	LDR R0, =empates
+	LDR R0, [R0]
+	bl puts
+	bl fin
+
+fin:
+	mov r0, #0
+	mov r3, #0
+	ldmfd sp!, {lr}
+	bx lr
