c2ea6574472d9e4a8c5b06d7ef3fe304e8c46bd2
Darlight <andres.perdomo09@gmail.com>
2019-04-25 07:17:03 -0600

    Palabras creadas

--- Fuga_palabras.s
+++ Fuga_palabras.s
@@ -30,4 +30,52 @@ num1:
 	.word 8
 num2:
 	.word 3
-	
+pal1:	
+	.asciz "p","a","c","t","o","_"
+pal2:
+	.asciz "c","a","l","l","e","_"
+pal3:
+	.asciz "r","a","d","i","o","_"
+pal4:
+	.asciz "t","r","e","c","e","_"
+pal5:
+	.asciz "d","a","m","a","s","_"
+pal6:
+	.asciz "q","u","e","j","a","_"
+pal7:
+	.asciz "p","e","c","a","s","_"
+pal8:
+	.asciz "m","a","c","h","o","_"
+pal9:
+	.asciz "r","a","b","i","a","_"
+pal10:
+	.asciz "j","a","d","e","o","_"
+pal11:
+	.asciz "m","a","d","r","e","_"
+pal12:
+	.asciz "i","d","o","l","o","_"
+pal13:
+	.asciz "f","a","c","h","a","_"
+pal14:
+	.asciz "b","a","h","i","a","_"
+pal15:
+	.asciz "d","a","n","g","o","_"
+pal16:
+	.asciz "j","a","b","o","n","_"
+pal17:
+	.asciz "j","o","r","g","e","_"
+pal18:
+	.asciz "v","a","g","o","s","_"
+pal19:
+	.asciz "a","b","e","j","a","_"
+pal20:
+	.asciz "q","u","e","s","o","_"
+
+
+
+
+
+
+
+
+
