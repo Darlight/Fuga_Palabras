ddd83d007e3b91a50962fcdf4fb21fa033383ac4
JosueS22 <35278085+JosueS22@users.noreply.github.com>
2019-04-25 06:51:05 -0600

    formatos

--- Fuga_palabras.s
+++ Fuga_palabras.s
@@ -6,6 +6,19 @@ Seccion 10 - Org. de computadoras y Assembler
 Proposito: Funciones vasicas con lenguaje ARM
 */
 
+.data
+.align 2
+
+bienvenida:	.asciz "BIENVENIDOS A FUGA DE PALABRAS\n"
+turno_jugador1:	.asciz "***TURNO JUGADOR 1***\n"
+turno_jugador2:	.asciz "***TURNO JUGADOR 2***\n"
+formato_palabra:.asciz "Palabra: %s\n"
+formato_vocal:	.asciz "Ingrese la vocal: %s\n"
+formato_mal1:	.asciz "ERROR. Puntos Jugador 1: %d\n"
+formato_mal2:	.asciz "ERROR. Puntos Jugador 2: %d\n"
+formato_bien1:	.asciz "BIEN. Puntos Jugador 1: %d\n"
+formato_bien2:	.asciz "BIEN. Puntos Jugador 2: %d\n"
+
 .text
 .align 2
 .global main
@@ -14,20 +27,5 @@ Proposito: Funciones vasicas con lenguaje ARM
 main:
 
 
-.data
-.align 2
-formato:
-	.asciz "La suma es %d\n"
-formato2:
-	.asciz "La resta es %d\n"
-formato3:
-	.asciz "La multiplicacion es %d\n"
-formato4:
-	.asciz "El AND de ambos registros es %d\n"
-formato5:
-	.asciz "OR de estos dos registros es  %d\n"
-num1:
-	.word 8
-num2:
-	.word 3
+
 	
